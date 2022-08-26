73f9dffa754dde4f1a50a37f4da355820e719637
NEAMIX\abdal <abdalrhmanhussin44@gmail.com>
2022-08-26 15:44:45 +0200

    Role Priviledges system

---
+++ src/components/modals/EditRoleModal.vue
@@ -0,0 +1,166 @@
+<template>
+    <div
+        class="overview fixed z-1000 w-full h-full-screen bg-shadow top-0 left-0 flex justify-center  items-center">
+        <Transition name="menu" appear>
+            <div class="modal bg-white rounded-md min-w-992 max-w-lg grid grid-cols-1" v-if="model.model_open">
+                <div class="p-5 relative">
+                    <h2 class="font-extrabold text-headline ">Setup your power packages</h2>
+                    <div class="mt-4 h-full">
+                        <div class="user_info">
+                            <label class="label pb-1">Role name</label>
+                            <input type="name" class="input w-full" placeholder="TheGreatestAssistance@iamgreat.com" v-model="payload.name"/>
+                            <p class="error">{{ error.name }}</p>
+                            <p class="error">{{ error.priviledge }}</p>
+                        </div>
+                        <div class="grid grid-cols-3 mt-3">
+                           <div class="group mt-4" v-for="priviledge in priviledges" :key="priviledge.id">
+                                <h2 class="text-xs mb-2 font-semibold">{{ priviledge.parent }}</h2>
+                                <div class="permission">
+                                    <div class="flex">
+                                        <label class="switch">
+                                            <input type="checkbox" :value="priviledge.id" @input="pushPriviledges(priviledge.id,$event)" v-model="this.payload.priviledges">
+                                            <span class="slider round"></span>
+                                        </label>
+                                        <p class="text-xs p-1 ">{{ priviledge.name }}</p>
+                                    </div>
+                                    <div class="children ml-3 mt-2">
+                                        <div class="permission flex mt-2" v-for="child in priviledge.children" :key="child.id">
+                                            <label class="switch">
+                                                <input type="checkbox" :value="child.id" :disabled="disabledPriviledges(child.parent_id)" v-model="this.payload.priviledges">
+                                                <span class="slider round"></span>
+                                            </label>
+                                            <p class="text-xs p-1 ">{{ child.name }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                           </div>
+                        </div>
+                        <div class="flex relative bottom-0 justify-end">
+                            <button class="relative right-2 my-5 mx-1 text-xs font-semibold bg-blue-600 py-2 px-10 rounded-sm text-white flex" @click="updateRole()" :disabled="loading.role_upsert">
+                                Update role
+                                <svg class="loader-auth absolute w-5" viewBox="0 0 50 50" v-if="loading.role_upsert"  style="right:7%">
+                                    <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+                                </svg>
+                            </button>
+                            <button class=" right-2 my-5 mx-1 text-xs font-semibold bg-red-600 py-2 px-10 rounded-sm text-white flex" @click="closeModel()">
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Transition>
+        <div class="model_loader" v-if="loading.model"></div>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'pinia';
+import { useGuardStore } from '../../stores/Guard';
+import { useCollectionStore } from '../../stores/collection';
+export default {
+    props: {
+        role_id: Number
+    },
+    data() {
+        return {
+            model: {
+                model_open: false,
+            },
+            loading: {
+                model: false,
+                role_upsert: false,
+            },
+            payload: {
+                priviledges: [],
+                name: ""
+            },
+            error: {
+                name: "",
+                priviledge: ""
+            },
+            dropdown: {
+                roleMenu: false
+            },
+            priviledges: [],
+            editRolePayload: {
+                id: 2
+            }
+        }
+    },
+    methods: {
+        ...mapActions(useGuardStore,['getRole','getPriviledges','upsertRole']),
+        closeModel() {
+            this.model.model_open = false;
+            this.$emit('close','update_role_model');
+        },
+
+        pushPriviledges(priviledge_id) {
+            let children_ids = this.getChildrenIds(priviledge_id);
+            let index = this.payload.priviledges.indexOf(priviledge_id);
+
+            if ( index != -1 ) {
+                this.payload.priviledges.splice(index,1);
+            }
+
+            for(var children_id of children_ids) {
+                let index = this.payload.priviledges.indexOf(children_id);
+                if ( index != -1 ) {
+                    this.payload.priviledges.splice(index,1);
+                }
+            }
+        },
+
+
+        updateRole() {
+            this.loading.role_upsert = true;
+            this.upsertRole(this.payload).then((response) => {
+                this.loading.role_upsert = false;
+                if ( response.data.errors ) {
+                    let data = response.data.errors[0].extensions.validation;
+                    this.error.name  = data['input.name'] ? data['input.name'][0] : null;
+                    this.error.priviledge = data['input.priviledge'] ? data['input.priviledge'][0] : null;
+                } else {
+                    this.model.model_open = false; 
+                    this.$emit('close','update_role_model');
+                    this.payload = {
+                        name: "",
+                        priviledges: [],
+                        id: null
+                    }
+                }
+            });
+        },
+
+        getChildrenIds(priviledge_id) {
+            let priviledge = this.priviledges.find((x) => x.id == priviledge_id);
+
+            if ( priviledge ) {
+                return priviledge.children.map((x) => x.id);
+            }
+        },  
+
+        disabledPriviledges(priviledge_parent_id) {
+            return (this.payload.priviledges.includes(priviledge_parent_id)) ? false : true;        
+        }
+    },
+    mounted() {
+        this.loading.model = true;
+        this.getPriviledges().then((response) => {
+            this.priviledges = response.data.data.priviledges;
+            this.getRole(this.role_id).then((response) => {
+                let exist_priviledges = response.data.data.role.priviledges
+                this.payload.name = response.data.data.role.name;
+                 this.payload.id = response.data.data.role.id;
+                let priviledge_array = [];
+                for ( var item of exist_priviledges ) {
+                    priviledge_array.push(item.id);
+                }
+                this.payload.priviledges = priviledge_array;
+                this.loading.model = false;
+                this.model.model_open = true;
+            });
+        });
+    }
+}
+</script>
--- src/stores/Guard.js
+++ src/stores/Guard.js
@@ -1,26 +1,170 @@
 import { defineStore } from "pinia";
 import {useAuthStore} from './Auth.js';
+import axios from "axios";
 
 export const useGuardStore = defineStore('guard', {
     state: () => {
         return {
-           priviledges: useAuthStore().user.priviledges
+           auth: useAuthStore().$state,
+           auth_token: localStorage.getItem('auth_token'),
         }
     },
     getters: {
        
     },
     actions: {
-       hasPriviledges(name) {
+        hasPriviledges(name) {
+            let priviledges = this.auth.user.role.priviledges;
+            if ( this.auth.user.role.id == 1 || this.auth.user.role.id == 2 ) {
+                return true;
+            }
+
             if ( name == 'role' ) {
-                return this.priviledges.includes(7 || 8)
+                return priviledges.find(x => x.id == 8 || 7);
             }
 
             if ( name == 'colleges' ) {
-                return this.priviledges.some(x => x.id == 1 || x.id == 2 )
+                return priviledges.find(x => x.id == 1 || x.id == 2 )
             }
 
             return true;
-       }
+        },
+
+        getPriviledges() {
+            return axios({
+                method: "POST",
+                url: import.meta.env.VITE_APP_END_POINT+'/graphql',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${this.auth_token}`
+                },
+                data: {
+                    query: `
+                    query {
+                        priviledges {
+                           name,
+                           parent,
+                           id,
+                           parent_id
+                           children {
+                             name,
+                             id,
+                             parent_id
+                          }
+                        }
+                    }
+                    `
+                }
+            })
+        },
+
+        arrification(json) {
+            let array = [];
+
+            for (var key in json){
+                var value = json[key];
+                array.push(value);
+            }
+
+            return array;
+        },
+
+        upsertRole(payload) {
+
+            return axios({
+                method: "POST",
+                url: import.meta.env.VITE_APP_END_POINT+'/graphql',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${this.auth_token}`
+                },
+                data: {
+                    query: `
+                    mutation roleupsert($name: String,$priviledges: [ID],$id: ID) {
+                        roleupsert(input: {name: $name,priviledges: $priviledges,id: $id}){
+                            name
+                        }
+                    }`,
+                    variables: {
+                        name: payload.name,
+                        priviledges: payload.priviledges,
+                        id: payload.id ?? null
+                    }
+                }
+            })
+        },
+
+        getRole(role_id) {
+            return axios({
+                method: "POST",
+                url: import.meta.env.VITE_APP_END_POINT+'/graphql',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${this.auth_token}`
+                },
+                data: {
+                    query: `
+                    query {
+                        role(id: ${role_id}) {
+                          name,
+                          id,
+                          priviledges {
+                           id,
+                        }
+                       }
+                    }
+                    `
+                }
+            });
+        },
+
+        getRolesList(search) {
+            return axios({
+                    method: "POST",
+                    url: import.meta.env.VITE_APP_END_POINT+'/graphql',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${this.auth_token}`
+                    },
+                    data: {
+                        query: `
+                        query {
+                            roles(input:{
+                                name: "${search.name}"
+                              }) {
+                               data {
+                                 name,
+                                 id
+                                 users {
+                                   name
+                                }
+                              }
+                            }
+                          }
+                        `
+                    }
+            })
+        },
+
+        deleteRole(payload) {
+            return axios({
+                method: "POST",
+                url: import.meta.env.VITE_APP_END_POINT+'/graphql',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${this.auth_token}`
+                },
+                data: {
+                    query: `
+                    mutation {
+                        roledelete(id: ${payload.id},password:"${payload.password}") {
+                            name
+                        }
+                    }
+                    `
+                }
+            })
+        }
+
     }
 })
