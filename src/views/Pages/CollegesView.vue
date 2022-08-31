b1d9e04f3fce48e822381de6233f9527e08500e8
NEAMIX\abdal <abdalrhmanhussin44@gmail.com>
2022-08-31 12:31:46 +0200

    fix ui of college

--- src/views/Pages/CollegesView.vue
+++ src/views/Pages/CollegesView.vue
@@ -11,10 +11,8 @@
                     <span>Colleges</span>
                     <span class="text-xs text-gray-500 px-4 hover:text-blue-500 cursor-pointer" @click="model.college_invite_model = true">+ Add Colleges</span>
                 </h2>
-                <!-- <button class="bg-silvery border-light-silvery px-5 py-1 text-xs rounded-xl font-semibold">Search Options</button> -->
                 <div class="xl:flex filter-search grid grid-cols-1">
                     <div class="search-container ml-2 md:ml-0">
-                        <label class="block text-sm text-xs font-lightbold pl-1 py-1">Search College</label>
                         <svg viewBox="64 64 896 896" focusable="false" data-icon="search" width="1em" height="1em"
                             fill="#5e5e5e" aria-hidden="true" class="absolute mt-2 w-4 text-md w-5 ml-2"
                             style="width: 13px; margin:9px">
@@ -24,15 +22,23 @@
                         </svg>
                         <input type="search" class="bg-gray-50 text-sm px-3 py-2 min-w-fit search" placeholder="Search.." @keyup="searchBy()" @change="searchBy()" v-model="search.name"/>
                     </div>
-                    <div class="search-container ml-2">
-                        <label class="block text-sm text-xs font-lightbold pl-1  py-1">Role</label>
-                        <v-select :options="['Canada', 'United States']" placeholder="Filter by role" class="select-search">
-                        </v-select>
-                    </div>
-                    <div class="search-container ml-2">
-                        <label class="block text-sm text-xs font-lightbold pl-1  py-1">Record to</label>
-                        <input type="date" class="bg-gray-50 text-sm px-3 py-2 min-w-fit search"
-                            placeholder="Search Colleage.." />
+                    <div class="search-container mx-10">
+                        <div class="placeholder input w-full flex items-center relative rounded-full" data-menu="roleMenu" @click="dropDowns.roleMenu =! dropDowns.roleMenu;">
+                            <span data-menu="roleMenu"> {{ model.placeholder_role }} </span>
+                            <font-awesome-icon icon="chevron-down" class="ml-auto transition-all" :class="{'rotate-180': dropDowns.roleMenu}" data-menu="roleMenu" />
+                            <transition 
+                                enter-active-class="transform transition duration-500 ease-custom"
+                                enter-class="-translate-y-1/2 scale-y-0 opacity-0"
+                                enter-to-class="translate-y-0 scale-y-100 opacity-100"
+                                leave-active-class="transform transition duration-300 ease-custom"
+                                leave-class="translate-y-0 scale-y-100 opacity-100"
+                                leave-to-class="-translate-y-1/2 scale-y-0 opacity-0">
+                                <ul class="select_menu bg-gray-100 rounded-md mt-2 shadow-md absolute z-20 "
+                                    v-if="dropDowns.roleMenu">
+                                    <li class="text-xs font-semibold p-2 hover:bg-blue-500 hover:text-white cursor-pointer" v-for="role in roles" :key="role.id" @click="search.role = role.id;this.model.placeholder = role.name;searchBy();model.placeholder_role = role.name">{{ role.name }}</li>
+                                </ul>
+                            </transition>
+                        </div>
                     </div>
                     <ul class="ml-auto mt-6 flex md:justify-end w-full">
                         <button 
@@ -57,6 +63,7 @@
                 </div>
             </div>
             <div class="page_content">
+                <p class="w-full text-center text-sm text-gray-500 mt-5 font-semibold"  v-if="!loading.users && users.length == 0">Opps, we didn't get any data</p>
                 <Transition  name="menu">
                     <div class="mt-3 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2" v-if="!loading.users">
                         <div class="card mt-1 bg-white shadow-sm mx-2 rounded-md relative" v-for="user in users" :key="user.id">
@@ -93,7 +100,7 @@
                                     </span>
                                 </div>
                             </div>
-                    </div>
+                        </div>
                     </div>
                 </Transition>
                 <div class="flex justify-center w-full" v-if="loading.users">
@@ -107,56 +114,17 @@
                         </path>
                     </svg>
                 </div>
-                <PaginationComponent :pages="pages" @swipePage="swipePage" v-if="!loading.users"></PaginationComponent>
+                <PaginationComponent :pages="pages" @swipePage="swipePage" v-if="!loading.users && pages > 1"></PaginationComponent>
                 <div class="h-6"></div>
             </div>
             <Transition name="fade">
-                <AddCollegeModel v-if="model.college_invite_model" @close="closeModels" @register="registerNewUser"></AddCollegeModel>
+                <AddCollegeModel v-if="model.college_invite_model" @close="closeModels" @register="registerNewUser" :search="search"></AddCollegeModel>
             </Transition>
             <Transition name="fade">
-                <EditColleageModel v-if="model.editModel" @close="closeModels" @updated="updateUser" :user_id="editUserPayload.id"></EditColleageModel>
+                <EditColleageModel v-if="model.editModel" @close="closeModels" @updated="updateUser" :user_id="editUserPayload.id" :page="current_page"></EditColleageModel>
             </Transition>
             <Transition name="fade">
-                <div
-                    @click.self="model.deleteModel = false"
-                    class="overview fixed z-1000 w-full h-full-screen bg-shadow top-0 left-0 flex justify-center  items-center" v-if="model.deleteModel">
-                    <Transition name="menu" appear>
-                        <div class="modal bg-white rounded-md  min-w-500  max-w-md  p-3" v-if="model.deleteModel">
-                            <div class="flex items-center justify-center flex-col">
-                                <img src="@/assets/img/system/warning.png" class="w-20"/>
-                                <p class="font-semibold text-xs p-4 text-center">
-                                    Howdy, We have to warn you that this action is irreversable and the deleted data can't be retrived again
-                                </p>
-                            </div>
-                            <div class="form-group w-full my-4">
-                                <input type="password" placeholder="Confirm your password" class="w-full border-0  bg-gray-200 rounded-sm text-xs font-semibold" v-model="deleteUserPayload.password"/>
-                                <p class="error">{{ error.deletePassword }}</p>
-                            </div>
-                            <div class="flex ml-auto w-full justify-end">
-                                <button
-                                @click="model.deleteModel = false" 
-                                class=" border-0 bg-blue-600 hover:bg-blue-500 transition-all  text-white text-us font-semibold px-6  py-1 rounded-sm mx-2">
-                                    Cancel
-                                </button>
-                                <button 
-                                @click="removeUser();"
-                                class=" flex relative justify-center items-center border-0 bg-red-600 hover:bg-red-500 transition-all  text-white text-us font-semibold px-6  py-1 rounded-sm">
-                                    <span>Confirm</span>
-                                    <svg 
-                                        v-if="loading.delete"
-                                        version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                        x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" width="23px" class="ml-4 absolute right-1">
-                                        <path fill="#fff"
-                                        d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                                        <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50"
-                                            to="360 50 50" repeatCount="indefinite" />
-                                        </path>
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </Transition>
-                </div>
+                <DeleteCollegeModel v-if="model.deleteModel" :user_id="deleteUserPayload.id" :page="current_page" @deleted="removedUser"></DeleteCollegeModel>
             </Transition>
             
         </PageSlot>
@@ -171,19 +139,25 @@ import { useAuthStore } from '../../stores/Auth';
 import PaginationComponent from "../../components/fixed/PaginationComponent.vue";
 import AddCollegeModel from "../../components/modals/AddCollegeModel.vue";
 import EditColleageModel from "../../components/modals/EditColleageModel.vue";
+import { useGuardStore } from '../../stores/Guard';
+import DeleteCollegeModel from "../../components/modals/DeleteCollegeModel.vue";
 
 export default {
-    components: { PageSlot, PaginationComponent, AddCollegeModel, EditColleageModel },
+    components: { PageSlot, PaginationComponent, AddCollegeModel, EditColleageModel, DeleteCollegeModel },
     data() {
         return {
             url: import.meta.env.VITE_APP_END_POINT + '/',
+            current_page: 1,
+            roles: [],
             dropDowns: {
-                userRoleSelect: false
+                userRoleSelect: false,
+                roleMenu: false
             },
             model: {
                 college_invite_model: false,
                 deleteModel: false,
-                editModel: false
+                editModel: false,
+                placeholder_role: 'All Roles'
             },
             users: [
                
@@ -202,7 +176,6 @@ export default {
 
             deleteUserPayload: {
                 id: null,
-                password:""
             },
 
             editUserPayload: {
@@ -212,7 +185,6 @@ export default {
             search: {
                 name: "",
                 role: null,
-                page: parseInt(localStorage.getItem('CollegePager') || 1)
             },
 
             loading: {
@@ -226,8 +198,10 @@ export default {
             isLoading: true
         }
     },
+   
     methods: {
         ...mapActions(useAuthStore,['register','getUserList','deleteUser','toggleUserActive','downloadUsersFile']),
+        ...mapActions(useGuardStore,['getRolesList']),
         swipePage(page) {
             this.search.page = page;
             this.loading.users = true;
@@ -236,7 +210,7 @@ export default {
 
         getAllUserList()
         {
-            this.getUserList(this.search).then((response) => {
+            this.getUserList(this.search,this.current_page).then((response) => {
                 this.users = response.data.data.users.data;
                 this.pages = response.data.data.users.paginatorInfo.lastPage;
                 this.loading.users = false;
@@ -262,20 +236,10 @@ export default {
             }
         },
 
-        removeUser() {
-            this.loading.delete = true;
-            this.error.deletePassword = "";
-            this.deleteUser(this.deleteUserPayload).then((response) => {
-                let errors = response.data.errors;
-                if ( errors ) {
-                    let passwordError = errors.extensions.validation['input.password'][0];
-                    this.error.deletePassword = passwordError;
-                } else {
-                    this.model.deleteModel = false;
-                    this.getAllUserList();
-                }
-                this.loading.delete = false;
-            });
+        removedUser(response) {
+          this.model.deleteModel = false;
+          this.users = response.data.data.deleteUser.users.data;
+          
         },
 
         toggleUserActivateState(id)
@@ -287,7 +251,7 @@ export default {
 
         downloadFile($type)
         {
-            this.downloadUsersFile($type);
+            this.downloadUsersFile($type,this.search);
         },
 
         closeModels($model)
@@ -307,21 +271,30 @@ export default {
             this.getAllUserList();
         },
 
-        updateUser($user)
+        updateUser(response)
         {
             this.model.editModel = false;
-            this.getAllUserList();
+            this.users = response.data.data.upsert.users.data;
+        },
+
+        closeMenus(e) {
+
+            let data = e.target.getAttribute('data-menu');
+            if ( data != 'roleMenu' ) this.dropDowns.roleMenu = false;
         }
     },
 
-   
-
     mounted() {
-        this.getUserList(this.search).then((response) => {
+        this.getUserList(this.search,this.current_page).then((response) => {
             this.users = response.data.data.users.data;
             this.pages = response.data.data.users.paginatorInfo.lastPage;
-            this.isLoading = false;
-        })
-    }
+
+            this.getRolesList({name: ""}).then((response) => {
+                this.roles = response.data.data.roles.data;
+                this.isLoading = false;
+            });
+
+        });
+    },
 }
 </script>
